d87c915b4d34c1b494a603455121e2f75b5ec886
uri <364850+cmpdc@users.noreply.github.com>
2024-04-19 20:30:55 -0400

    update

--- src/client/scripts/pages/access/access.js
+++ src/client/scripts/pages/access/access.js
@@ -1,3 +1,8 @@
+import { goToPage } from "../../../app.js";
+import { addComponent } from "../../utils.js";
+
+// NOTE: don't forget that when importing modules, there needs to have ".js" at the end
+
 /**
  * The "account" page of our app.
  * This component is what the user will see to login or create account.
@@ -5,4 +10,17 @@
  */
 export const loadAccessPage = (element) => {
 	element.innerHTML = `<h1>Login/SignUp</h1>`;
+
+	const sampleButton = addComponent({
+		type: "button",
+		props: {
+			id: "goTo",
+			textContent: "Go to Dashboard",
+			onClick: (e) => {
+				goToPage("/dashboard");
+			},
+		},
+	});
+
+	element.appendChild(sampleButton);
 };
--- src/client/scripts/pages/landing/landing.js
+++ src/client/scripts/pages/landing/landing.js
@@ -1,3 +1,8 @@
+import { goToPage } from "../../../app.js";
+import { addComponent } from "../../utils.js";
+
+// NOTE: don't forget that when importing modules, there needs to have ".js" at the end
+
 /**
  * The "homepage" of our app.
  * This component is what will be seen if a user uses our app without an account.
@@ -5,4 +10,17 @@
  */
 export const loadLandingPage = (element) => {
 	element.innerHTML = `<h1>Home</h1>`;
+
+	const sampleButton = addComponent({
+		type: "button",
+		props: {
+			id: "goTo",
+			textContent: "Go to Dashboard",
+			onClick: (e) => {
+				goToPage("/dashboard");
+			},
+		},
+	});
+
+	element.appendChild(sampleButton);
 };
